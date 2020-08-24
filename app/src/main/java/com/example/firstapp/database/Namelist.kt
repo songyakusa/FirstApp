5590d7bbfe813e59668f1cd54f667f04c61008e6
CHARAN <songyakusa@gmail.com>
2020-08-25 04:21:00 +0700

    update build.gradle

--- app/src/main/java/com/example/firstapp/database/Namelist.kt
+++ app/src/main/java/com/example/firstapp/database/Namelist.kt
@@ -1,17 +1,15 @@
-package com.example.firstapp.database
+package com.example.newfirstapp.database
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "namelist_table")
-data class Namelist (
+data class Namelist(
 
     @PrimaryKey(autoGenerate = true)
-    var contactId: Long = 0L,
+    var namelistId: Long = 0L,
 
-    @ColumnInfo(name = "contactList_name")
-    var name:String
-
+    @ColumnInfo(name = "name")
+    var name: String
 )
--- app/src/main/java/com/example/firstapp/database/NamelistDao.kt
+++ app/src/main/java/com/example/firstapp/database/NamelistDao.kt
@@ -5,13 +5,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import com.example.newfirstapp.database.Namelist
 
 @Dao
 interface NamelistDao {
     @Insert
     fun insertName(namelist: Namelist)
 
-    @Query("SELECT * FROM namelist_table ")
+    @Query("SELECT * FROM namelist_table ORDER BY namelistId DESC")
     fun getName(): LiveData<List<Namelist>>
 
 }
